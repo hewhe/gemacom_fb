b34ff6d9367a87bb0c53028fb7d7a9a5dc26ed79
tama <yuki023201025@gmail.com>
2020-03-28 20:42:51 +0900

    新規登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        #@user.image_name = "d_image.jpg" 画像の保存
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:notice] = "登録！！"
+            redirect_to(root_path)
+        else
+            render(:new)
+        end
     end
 
     def show
@@ -12,5 +22,10 @@ class UsersController < ApplicationController
     end
 
     def update
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :profile, :image, :password)
     end
 end
