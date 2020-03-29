9ceadd8c66bbfad3c350afa76eac5fd5ea2f1a5b
tama <yuki023201025@gmail.com>
2020-03-29 15:14:53 +0900

    募集側の新規投稿と一覧表示が見れる

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,19 +1,35 @@
 class InvitesController < ApplicationController
     def index
+        @invites = Invite.all
     end
 
     def new
+        @invite = Invite.new
+        @invite_categories = InviteCategory.all
     end
 
     def create
+        @invite = Invite.new(invite_params)
+        if @invite.save
+            flash[:notice] = "投稿に成功しました"
+            redirect_to(invites_path)
+        else
+            render(:new)
+        end
     end
 
-    def edit
-    end
+    #機能制限ができないから実装できないかもしれない
+    # def edit
+    # end
 
-    def update
-    end
+    # def update
+    # end
 
-    def destroy
+    # def destroy
+    # end
+
+    private
+    def invite_params
+        params.require(:invite).permit(:title, :content, :invite_category_id)
     end
 end
