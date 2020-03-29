1e3be7d49e2314a561a1b00788edac677c6c7ca0
tama <yuki023201025@gmail.com>
2020-03-29 12:49:35 +0900

    ログイン機能の追加とログイン関連のメソッドの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+    #ユーザとログインしているユーザが一致している時true
+    # def current_user?(user)
+    #     user == current_user
+    # end
+
+    #ログインしている時はtrueを返す（何もない(nil)の否定だから）
+	def logged_in?
+		!current_user.nil?
+    end
+
+	def authenticate_user
+		if current_user == nil
+			flash[:notice] = "ログインしてください"
+			redirect_to "/login"
+		end
+	end
+
+    def forbid_login_user
+        if current_user
+            flash[:notice] = "既にログインしています"
+            redirect_to(root_path)
+        end
+    end
 end
