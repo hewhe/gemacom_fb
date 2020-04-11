b1bbd16d88dc48a7c05bfa7d30ca9714d7954b33
tama <yuki023201025@gmail.com>
2020-04-11 17:40:32 +0900

    グループ作成画面のレイアウト

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
     has_many :members
     has_many :group_boards
     belongs_to :group_category
+
+    validates :name, presence: true
+    validates :group_category_id, presence: true
+    validates :image, presence: true
 end
