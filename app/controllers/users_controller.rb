737c77c02cb7bcd805c91133ec820a6b5bb8a1c0
tama <yuki023201025@gmail.com>
2020-04-13 11:30:22 +0900

    userデフォルト画像をpublicから移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        @user.image_id = "/112690.jpg"
+        @user.image_id = "112690.jpg"
         if @user.save
             session[:user_id] = @user.id
             flash[:success] = "登録に成功しました"
